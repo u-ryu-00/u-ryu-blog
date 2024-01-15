cfb548eccec15b3fb1e25e884073cfb3d516fcc3
u-ryu-00 <lwh76104@gmail.com>
2024-01-15 16:58:36 +0900

    Supabase 연동

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,3 +1,4 @@
+import { createClient } from '@/utils/supabase/server';
 import { GetServerSideProps } from 'next';
 
 type PostProps = {
@@ -12,8 +13,17 @@ export default function Post({ id }: PostProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+  req,
+}) => {
   const { id } = query;
+
+  const supabase = createClient(req.cookies);
+
+  const response = await supabase.from('Post').select('*').eq('id', Number(id));
+
+  console.log(response);
 
   return {
     props: {
