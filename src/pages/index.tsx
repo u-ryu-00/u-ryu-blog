0206c38ae9bf380b69387030f396c65a4ad317ec
u-ryu-00 <lwh76104@gmail.com>
2024-01-23 16:37:56 +0900

    글 목록 무한 스크롤 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,52 @@
 import PostCard from '@/components/PostCard';
 import { createClient } from '@/utils/supabase/client';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 const supabase = createClient();
 
 export default function Home() {
-  const { data: posts } = useQuery({
+  const { ref, inView } = useInView();
+
+  const {
+    data: postPages,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
     queryKey: ['posts'],
-    queryFn: async () => {
+    queryFn: async ({ pageParam }) => {
       const { data } = await supabase
         .from('Post')
         .select('*')
-        .order('created_at', { ascending: false });
-      if (!data) return [];
-      return data;
+        .order('created_at', { ascending: false })
+        .range(pageParam, pageParam + 4);
+      if (!data)
+        return {
+          posts: [],
+          nextPage: null,
+        };
+      return {
+        posts: data,
+        nextPage: data.length === 5 ? pageParam + 5 : null,
+      };
     },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage) fetchNextPage();
+  }, [inView, hasNextPage, fetchNextPage]);
+
   return (
-    <div className="container mx-auto grid grid-cols-2 gap-x-4 gap-y-6 px-4 pb-24 pt-20 lg:gap-x-7 lg:gap-y-12">
-      {posts?.map((post) => <PostCard key={post.id} {...post} />)}
+    <div className="flex flex-col">
+      <div className="container mx-auto grid grid-cols-2 gap-x-4 gap-y-6 px-4 pb-24 pt-20 lg:gap-x-7 lg:gap-y-12">
+        {postPages?.pages
+          .flatMap((page) => page.posts)
+          .map((post) => <PostCard key={post.id} {...post} />)}
+      </div>
+      <div ref={ref} />
     </div>
   );
 }
