87f2153557c74f82ee70f3bc54aab046c42ec2f7
u-ryu-00 <lwh76104@gmail.com>
2024-01-25 14:56:40 +0900

    블로그 추가 기능 구현 및 리팩토

--- src/pages/tags/index.tsx
+++ src/pages/tags/index.tsx
@@ -1,22 +1,13 @@
-import { createClient } from '@/utils/supabase/client';
-import { useQuery } from '@tanstack/react-query';
+import { useTags } from '@/utils/hooks';
 import Link from 'next/link';
 
-const supabase = createClient();
-
 export default function Tag() {
-  const { data: existingTags } = useQuery({
-    queryKey: ['tags'],
-    queryFn: async () => {
-      const { data } = await supabase.from('Post').select('tags');
-      return Array.from(new Set(data?.flatMap((d) => JSON.parse(d.tags))));
-    },
-  });
+  const { data: existingTags } = useTags();
 
   return (
     <div className="flex flex-col items-center gap-2 px-4 pb-24 pt-20">
       <h1 className="mb-8 text-center text-2xl font-semibold">태그</h1>
-      <div className="container mx-auto flex flex-wrap justify-center gap-2 px-10">
+      <div className="container flex flex-wrap justify-center gap-2">
         {existingTags?.map((tag) => (
           <Link
             href={`/tags/${tag}`}
